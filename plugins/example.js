8007d7c0ac930e3032fe0e82a04def46a115ba17
IanSSenne <48780301+IanSSenne@users.noreply.github.com>
2021-05-29 03:47:15 -0700

    add gh action

--- plugins/example.js
+++ plugins/example.js
@@ -3,7 +3,6 @@ name = Hello World
 version = 0.0.1
 author = FetchBot
 description = A test plugin
-mount = postload
 url = www.example.com
 */
 
---
+++ scripts/build.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const path = require("path");
+
+fs.mkdirSync("./out", { recursive: true });
+fs.writeFileSync(
+  "./out/plugins.json",
+  JSON.stringify(
+    fs.readdirSync("./plugins").map((filepath) => {
+      const loc = path.resolve(process.cwd(), "plugins", filepath);
+      const file = fs.readFileSync(loc, "utf-8");
+      const lines = file.split("\n");
+      let meta = {};
+      for (let i = 0; i < lines.length; i++) {
+        if (lines[i].startsWith("/*meta")) {
+          for (let j = i + 1; j < lines.length; j++) {
+            if (lines[j] === "*/") {
+              if (
+                meta.name &&
+                meta.version &&
+                meta.author &&
+                meta.description
+              ) {
+                fs.copyFileSync(loc, "./out/" + filepath);
+                return meta;
+              } else {
+                throw new Error(`PLUGIN ${filepath} HAS INVALID META`);
+              }
+            }
+            let line = lines[j];
+            const key = line.substr(0, line.indexOf("=")).trim();
+            const value = line.substr(line.indexOf("=") + 1).trim();
+            meta[key] = value;
+          }
+        }
+      }
+      throw new Error(`NO META IN PLUGIN ${filepath}`);
+    })
+  )
+);
