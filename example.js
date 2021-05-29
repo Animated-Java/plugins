6c57b43a0b272071b49e265dad62a12c54554604
IanSSenne <IanSSenne@users.noreply.github.com>
2021-05-29 10:48:53 +0000

    deploy: 986346563304307a5d5412b88dd72a74167a4ee7

---
+++ example.js
@@ -0,0 +1,11 @@
+/*meta
+name = Hello World
+version = 0.0.1
+author = FetchBot
+description = A test plugin
+url = www.example.com
+*/
+
+(() => {
+  console.log("hi from an animated java plugin");
+})();
